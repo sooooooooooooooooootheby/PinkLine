af4d3faf7c12d05ec6dbad57b22222af0547c40e
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-03-21 12:40:59 +0800

    添加 萌备

--- components/appFooter.vue
+++ components/appFooter.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="footer">
-        <div class="copyright">Copyright ©2023~2025.sooooooooooooooooootheby</div>
+        <div class="info">
+            <div class="copyright">Copyright ©2023~2025.sooooooooooooooooootheby</div>
+            <a href="https://icp.gov.moe/?keyword=20250416" target="_blank" class="moeIcp">萌ICP备20250416号</a>
+        </div>
         <theme />
     </div>
 </template>
@@ -16,8 +19,13 @@
     justify-content: space-between;
     border-top: 1px solid var(--border-color);
 
-    .copyright {
-        opacity: 0.8;
+    .info {
+        .copyright {
+            opacity: 0.8;
+        }
+        .moeIcp {
+            color: var(--theme-color)
+        }
     }
 }
 
