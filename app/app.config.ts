597de301bc1be09bd74099c96a1dcbf90989d6bc
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-10-05 13:16:04 +0800

    feat: 配置文件添加注释

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,25 +1,28 @@
 export default defineAppConfig({
+	// 博客的基本信息
 	info: {
-		title: "PinkLine",
-		author: "s22y",
-		lang: "zh-cn",
-		logo: "/logo_MG.gif",
-		description: "基于nuxt.js的博客",
-		ogDescription: "基于nuxt.js的博客",
-		ogImage: "/Canvas-Ruom.webp",
+		title: "PinkLine",						// 标题
+		author: "s22y",							// 作者
+		lang: "zh-cn",							// 语言
+		logo: "/logo_MG.gif",					// logo icon
+		description: "基于nuxt.js的博客",		 // 简介
+		ogDescription: "基于nuxt.js的博客",		 // og 简介
+		ogImage: "/Canvas-Ruom.webp",			// og 图片
 	},
+	// 功能开关
 	switch: {
 		appHeader: {
-			isLogo: true,
-			isTitle: false,
+			isLogo: true,						// 是否显示导航栏的图标
+			isTitle: false,						// 是否显示导航栏的标题
 		},
-		comment: false,
+		comment: false,							// 是否启用评论区
 	},
+	// 联系方式
 	contact: [
 		{
-			platform: "Discord",
-			url: "https://pinkline.s22y.moe/",
-			icon: "akar-icons:coffee",
+			platform: "Discord",				// 平台
+			url: "https://pinkline.s22y.moe/",	// 链接
+			icon: "akar-icons:coffee",			// 图标 图标库: https://yesicon.app/
 		},
 		{
 			platform: "Discord",
@@ -37,12 +40,13 @@ export default defineAppConfig({
 			icon: "akar-icons:coffee",
 		},
 	],
+	// 项目
 	project: [
 		{
-			name: "Blog",
-			info: "这是一个博客.",
-			url: "https://pinkline.s22y.moe/",
-			time: "Wed Mar 10 2025 15:53:43 GMT+0800",
+			name: "Blog",								// 名字
+			info: "这是一个博客.",						 // 介绍
+			url: "https://pinkline.s22y.moe/",			// 链接
+			time: "Wed Mar 10 2025 15:53:43 GMT+0800",	// 时间 支持UTC和ISO格式
 		},
 		{
 			name: "Blog",
@@ -63,13 +67,20 @@ export default defineAppConfig({
 			time: "Wed Mar 10 2025 15:53:43 GMT+0800",
 		},
 	],
+	// 页面
 	page: [
-		{ path: "/", name: "S22y", title: "", icon: "akar-icons:coffee" },
+		{
+			path: "/",					// 路径
+			name: "S22y",				// 名字
+			title: "",					// 显示的名字
+			icon: "akar-icons:coffee"	// 图标 (暂时没用)
+		},
 		{ path: "/blog", name: "Blog", title: "Blog", icon: "akar-icons:coffee" },
 		{ path: "/note", name: "Note", title: "Note", icon: "akar-icons:coffee" },
 		{ path: "/friends", name: "Friends", title: "Friends", icon: "akar-icons:coffee" },
 	],
+	// rss
 	rss: {
-		link: "blog.s22y.moe",
+		link: "blog.s22y.moe",			// 填写你博客的域名即可
 	},
 });
